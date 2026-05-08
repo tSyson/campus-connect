f0deafc000a9234c6af7cff8e54690431d6db619
Ben <bendictokawagga@gmail.com>
2026-05-08 12:11:30 +0300

    update on package-lock.json, package.json and vite.comfig.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig(({ mode }) => ({
     hmr: {
       overlay: false,
     },
+    allowedHosts: ["campus-connect-u1iy.onrender.com"],
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
